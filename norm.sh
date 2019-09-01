f0186da18c1a787c8eb2121a0549b3d32ccdc7e1
dreamguo <836918658@qq.com>
2019-09-01 17:49:11 +0800

    resnet

--- norm.sh
+++ norm.sh
@@ -1,5 +1,5 @@
 #my.sh
 #!/bin/bash
 
-python3 normalize.py --feature_dir ../i3d/rgb_oversample_4/ --output_dir ../i3d/rgb_oversample_4_norm/
+python3 normalize.py --feature_dir ../i3d/flow_oversample_4/ --output_dir ../i3d/flow_oversample_4_norm/
 
---
+++ resnet.sh
@@ -0,0 +1,5 @@
+#my.sh
+#!/bin/bash
+
+python3 extract_ResNet.py --video_dir ../New_video/Full --output_dir ../resnet/
+
